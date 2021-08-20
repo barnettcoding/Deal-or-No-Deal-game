4843ae5b3f8d331d27ccc988fb5bfca84820670c
barnettcoding <89255759+barnettcoding@users.noreply.github.com>
2021-08-20 10:50:12 -0400

    Add files via upload

--- deal_or_no_deal.py
+++ deal_or_no_deal.py
@@ -7,33 +7,33 @@ my_box = boxes.pop(my_choice - 1)
 boxes_left = len(boxes)
 game_over = False
 #choose a box
-for box in boxes:
-    str.format({":,.2f"}) 
 for i in range(1,14):
-    my_choice = int(input(f"Choose a box to get rid of: 1 throuh {boxes_left}\n"))
-    print(f"You got rid of ${boxes[my_choice-1]} dollars\n")
+    my_choice = int(input(f"Choose a box to get rid of: 1 through {boxes_left}\n"))
+    f_box = "${:,}".format(boxes[my_choice-1])
+    print(f"You got rid of {f_box} dollars\n")
     boxes.pop(my_choice - 1)
     boxes_left = len(boxes)    
     if i % 3 == 0:
 #offer from bankers
         offer = sum(boxes) / len(boxes)
+        f_offer = "${:,.2f}".format(offer)
         print("Ringggg, Ringgggg")
         print("We have a call from the bankers.")
-        print(f"They have offered you {offer} for your case")
+        print(f"They have offered you {f_offer} for your case")
         acceptance = input("Do you accept? Type 'Deal' or 'No Deal'\n").lower()
         if acceptance == "no deal":
             print("No deal, let's keep playing!\n")
             continue
         elif acceptance == "deal":
-            print(f"Congratulations! You won {offer}!")
+            print(f"Congratulations! You won {f_offer}!")
             game_over = True
             break
         else: 
             print("Invalid choice")
 #endgame
 if not game_over:
-    print(f"You have 12 boxes left.\nYou can keep your box or\nchoose to keep one of the six boxes left on the board.")
-    my_choice = input("Which will you choose?\nTo keep your box type 'my box'.\nOtherwise type a number between '1' and '6'\n")
+    print(f"You have 12 boxes left.\nYou can keep your box or\nchoose to keep one of the twelve boxes left on the board.")
+    my_choice = input("Which will you choose?\nTo keep your box type 'my box'.\nOtherwise type a number between '1' and '12'\n")
     if my_choice == "my box":
         if my_box > 1000: 
             print(f"Congratulations! You kept your original box and ended up with ${my_box}!")
@@ -45,12 +45,3 @@ if not game_over:
             print(f"Congratulations! You chose box {my_choice} and ended up with ${boxes[my_choice - 1]}!")
         else:
             print(f"Well, you only ended up with ${boxes[(my_choice - 1)]}.\nMaybe you should've trusted what was in your own box!")
-
-
-
-
-
-
-
-
-
